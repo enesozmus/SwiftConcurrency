b1ebefedc60d4c5fc342fc2c79d76cdb0e63a3fe
enesozmus <ensozmus@gmail.com>
2024-06-20 13:19:55 +0300

    Initial Commit

---
+++ SwiftConcurrency/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftConcurrency
+//
+//  Created by enesozmus on 20.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftConcurrency/SwiftConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyApp.swift
+//  SwiftConcurrency
+//
+//  Created by enesozmus on 20.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
